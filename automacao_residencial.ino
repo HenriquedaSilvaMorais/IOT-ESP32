afe6a94014129cb05bf644d7f81f1b3a07e671f9
Henrique da Silva Morais <61944160+HenriquedaSilvaMorais@users.noreply.github.com>
2022-02-14 20:07:36 -0300

    Add files via upload

---
+++ automacao_residencial.ino
@@ -0,0 +1,70 @@
+//HENRIQUE DA SILVA MORAIS 
+//ESP-32
+
+// digital pin 2
+#define LED_PIN 2
+
+// set up the 'digital' feed
+AdafruitIO_Feed *digital = io.feed("digital");
+
+void setup() {
+  
+  pinMode(LED_PIN, OUTPUT);
+  
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // set up a message handler for the 'digital' feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  digital->onMessage(handleMessage);
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+  digital->get();
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+}
+
+// this function is called whenever an 'digital' feed message
+// is received from Adafruit IO. it was attached to
+// the 'digital' feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *data) {
+
+  Serial.print("received <- ");
+
+  if(data->toPinLevel() == HIGH){
+    Serial.println("HIGH");
+    digitalWrite(LED_PIN,HIGH);
+    }
+  else{
+    Serial.println("LOW");
+    digitalWrite(LED_PIN,HIGH);
+  }
+
+  digitalWrite(LED_PIN, data->toPinLevel());
+}
